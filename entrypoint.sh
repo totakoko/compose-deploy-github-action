c0b8207a88d26a2c6c4589ff52c9215e46f25d5a
Maxime Dréau <maxime@dreau.fr>
2023-03-04 21:48:57 +0100

    Fix script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,26 +1,5 @@
 #!/usr/bin/env sh
 
-# exec docker run \
-#   -v "/var/run/docker.sock":"/var/run/docker.sock" \
-#   -e SSH_HOST \
-#   -e SSH_PORT \
-#   -e SSH_USER \
-#   -e SSH_FINGERPRINT \
-#   -e SSH_PRIVATE_KEY \
-#   -e CRYPT_KEY_BASE64 \
-#   -v ${{ github.workspace }}:/modules \
-#   -w /modules \
-#   totakoko/compose-deploy:latest deploy \
-#   -c "
-# git config --global --add safe.directory /modules
-# compose-deploy deploy
-# "
-
 # fix error about dubious ownership in repository at '/github/workspace'
-id
-pwd
-ls -la
-ls -l /modules
-ls -l /github/workspace
-git config --global --add safe.directory /modules
-compose-deploy deploy
+git config --global --add safe.directory $PWD
+MODULES_ROOT=$PWD exec compose-deploy deploy
