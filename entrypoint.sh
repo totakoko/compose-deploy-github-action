dce7d590d824bd786b5c9deba88a9e61c8d3d510
Maxime Dréau <maxime@dreau.fr>
2023-03-04 21:50:54 +0100

    Fix rights

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env sh
 
 # fix error about dubious ownership in repository at '/github/workspace'
-git config --global --add safe.directory $PWD
+chown -R root: .
+
 MODULES_ROOT=$PWD exec compose-deploy deploy
