66ab7d3df3ebf8522b1b431ba867b0b34224540e
Maxime Dréau <maxime@dreau.fr>
2023-03-04 22:33:51 +0100

    Disable password auth

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,7 @@
 # fix error about dubious ownership in repository at '/github/workspace'
 git config --global --add safe.directory $PWD
 
+echo 'PasswordAuthentication no' >> /etc/ssh/ssh_config
+
 echo $SSH_HOST $SSH_PORT $SSH_FINGERPRINT | base64
 ANSIBLE_ARGS=-vvvv MODULES_ROOT=$PWD exec compose-deploy deploy
